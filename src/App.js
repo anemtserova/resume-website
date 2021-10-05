d9e1ca4e580f28d3f5c49bb4bd0272dd3f93f78f
unknown <anemtserova@gmail.com>
2021-10-05 08:48:02 -0400

    working on

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import logo from './logo.svg';
 import './App.css';
+import { Navbar } from './Components/navbar';
 
 const App = () => {
 	return (
 		<div className="App">
+			<Navbar />
 			<header className="App-header">
 				<p>I'm gonna try to deploy this on Heroku now.</p>
 			</header>
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export const Navbar = () => {
 	return (
-		<div className="d-flex justify-content-between">
+		<div className="d-flex flex-row justify-content-between">
 			<div>My Navbar</div>
-			<div className="d-flex justify-content-center">
-				<div>
-					<Link to="/">
-						<button>Home</button>
-					</Link>
-				</div>
-				<div>
-					<Link to="/about">
-						<button>About</button>
-					</Link>
-				</div>
-				<div>
-					<Link to="/work">
-						<button>Projects</button>
-					</Link>
-				</div>
+			<div className="d-flex flex-row justify-content-center">
+				<Router>
+					<div className="d-flex">
+						<Link to="/">
+							<button>Home</button>
+						</Link>
+					</div>
+					<div className="d-flex">
+						<Link to="/about">
+							<button>About</button>
+						</Link>
+					</div>
+					<div className="d-flex">
+						<Link to="/work">
+							<button>Projects</button>
+						</Link>
+					</div>
+				</Router>
 			</div>
 		</div>
 	);
